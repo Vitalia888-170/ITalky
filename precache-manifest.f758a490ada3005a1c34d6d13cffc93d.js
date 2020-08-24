eda541ccd0f7c9718c80adb27af0489999b41c11
Vitaliia <64986388+Vitalia888-170@users.noreply.github.com>
2020-08-24 19:18:31 +0300

    Updates

---
+++ precache-manifest.f758a490ada3005a1c34d6d13cffc93d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e74959a2e796df2ff326",
+    "url": "/ITalky/static/css/main.ef12d3e8.chunk.css"
+  },
+  {
+    "revision": "e74959a2e796df2ff326",
+    "url": "/ITalky/static/js/main.e74959a2.chunk.js"
+  },
+  {
+    "revision": "d82fe7ff194910d64523",
+    "url": "/ITalky/static/js/runtime~main.d82fe7ff.js"
+  },
+  {
+    "revision": "8508ca54db00288998f3",
+    "url": "/ITalky/static/js/2.8508ca54.chunk.js"
+  },
+  {
+    "revision": "869b8084abcf11ef6bf8",
+    "url": "/ITalky/static/css/3.df02354e.chunk.css"
+  },
+  {
+    "revision": "869b8084abcf11ef6bf8",
+    "url": "/ITalky/static/js/3.869b8084.chunk.js"
+  },
+  {
+    "revision": "34479230f4cc130a85c4",
+    "url": "/ITalky/static/css/4.858c4a89.chunk.css"
+  },
+  {
+    "revision": "34479230f4cc130a85c4",
+    "url": "/ITalky/static/js/4.34479230.chunk.js"
+  },
+  {
+    "revision": "04aba51cd8d50df4ac7b8ff36fed8f6f",
+    "url": "/ITalky/static/media/logo.04aba51c.jpg"
+  },
+  {
+    "revision": "829c06d2479cb1c952848126ea45e95a",
+    "url": "/ITalky/static/media/preloader.829c06d2.svg"
+  },
+  {
+    "revision": "0b856d3ab596c6bee3f2e9c6d61ca236",
+    "url": "/ITalky/static/media/login1.0b856d3a.svg"
+  },
+  {
+    "revision": "8939df9c60f296e459951ef1fc058675",
+    "url": "/ITalky/index.html"
+  }
+];
